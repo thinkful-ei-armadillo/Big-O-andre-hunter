a73c441e6172b852d63b0521adc670627cef5464
Hunter Kreshock <hkreshock@gmail.com>
2019-04-02 09:56:37 -0700

    1-4 drills

---
+++ big-o.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// function jumpingSheep(num) {
+//   for (let i = num; i >= 0; i--) {
+//     if (i === 0) {
+//       console.log('All sheep jumped over the fence');
+//     } else {
+//       console.log('Another sheep jumps over the fence');
+//     }
+//   }
+// }
+
+// console.log(jumpingSheep(3));
+
+// function power(num) {
+//   console.log(Math.pow(10, num));
+// }
+
+// console.log(power(3));
+
+// function reverse(str) {
+//   let arr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     arr.push(str[str.length - 1 - i]);
+//   }
+//   return arr.join('');
+// }
+
+// console.log(reverse('hunter'));
+
+// function makeTri(num) {
+//   let number = 0;
+//   for (let i = num; i >= 0; i--) {
+//     number = number + i;
+//   }
+//   return number;
+// }
+
+// console.log(makeTri(4));
+
